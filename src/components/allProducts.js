fd01f5f7ecbef35dcf3fad63558fc10ce8abb096
Suparoek Taetiangthum <jsuparoek@Suparoeks-MBP.attlocal.net>
2023-01-28 10:55:23 -0600

    all product

--- src/components/allProducts.js
+++ src/components/allProducts.js
@@ -13,17 +13,30 @@ const AllProducts = ({products}) => {
         <SideBar all={products} products={filteredProducts} setFilter={setFilteredProducts} />
 
         <div className="card-outline">
-          {filteredProducts.length ? (
-            filteredProducts.map(product => {
-              return (
-                <div key={product.id}>
-                  <CardView product={product} />
-                </div>
-              );
-            })
-          ) : (
-            <h1>No products!</h1>
-          )}
+          {filteredProducts.length > 2 && filteredProducts.map(product => {
+            return (
+              <div key={product.id}>
+                <CardView product={product} />
+              </div>
+            );
+          })}
+          {filteredProducts.length === 2 && <>
+            <div key={filteredProducts[0].id}>
+              <CardView product={filteredProducts[0]} />
+            </div>
+            <div key={filteredProducts[1].id}>
+              <CardView product={filteredProducts[1]} />
+            </div>
+            <div className="card-empty" />
+          </>}
+          {filteredProducts.length === 1 && <>
+            <div key={filteredProducts[0].id}>
+              <CardView product={filteredProducts[0]} />
+            </div>
+            <div className="card-empty" />
+            <div className="card-empty" />
+          </>}
+          {!filteredProducts.length && <h1 className="no-product">No products!</h1>}
         </div>
       </div>
     </div>
